6af94790101db1f82665f6db5ba60582ffe57cb5
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2023-12-17 18:19:31 +0300

    driver fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,9 +9,10 @@ def browser_management():
     browser.config.window_height = 900
     # browser.config.type_by_js = True
     browser.config.base_url = 'https://demoqa.com'
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-    browser.config.driver_options = driver_options
+    browser.config.driver_name = 'chrome'
+    # driver_options = webdriver.ChromeOptions()
+    # driver_options.page_load_strategy = 'eager'
+    # browser.config.driver_options = driver_options
 
     yield
 
